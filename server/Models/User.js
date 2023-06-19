afcddb94f83282c69a37e2b7987db78d0fd401c3
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-19 11:09:31 -0700

    Create User.js

---
+++ server/Models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const schema = new mongoose.Schema({
+    name: String,
+    photo: String,
+    googleId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    role: {
+        type: "String",
+        enum: ["admin", "user"],
+        default: "user",
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export const User = mongoose.model("User", schema)
