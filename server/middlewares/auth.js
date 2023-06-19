0990fef929c73b8355d709f3c9ff8074b6183e6e
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-19 11:16:47 -0700

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,4 +1,4 @@
-import ErrorHandler from "../utils/ErrorHandler.js";
+import ErrorHandler from "../Utils/ErrorHandler";
 
 export const isAuthenticated = (req, res, next) => {
   const token = req.cookies["connect.sid"];
