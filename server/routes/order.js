df3bbcc375eaa1e4c7af3db743810d24a87de6a5
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-22 11:18:47 -0700

    Update order.js

--- server/routes/order.js
+++ server/routes/order.js
@@ -10,3 +10,15 @@ import {
 } from "../controllers/order.js";
 import { authorizeAdmin, isAuthenticated } from "../middlewares/auth.js";
 
+const router = express.Router()
+
+router.post("/createorder", isAuthenticated, placeOrder)
+
+router.post("/createorderonline", isAuthenticated, placeOrderOnline)
+
+router.post("/paymentverification", isAuthenticated, paymentVerification)
+
+router.get("/myorders", isAuthenticated, getMyOrders);
+
+router.get("/order/:id", isAuthenticated, getOrderDetails)
+
