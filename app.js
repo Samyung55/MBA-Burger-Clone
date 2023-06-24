6dfdacc481172943207489784b9250851a11a49e
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-24 13:31:36 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const express, { urlencoded } = require("express")
+const dotenv = require("dotenv")
+const { connectPassport } = require("./utils/Provider.js")
+const session = require("express-session")
+const cookieParser = require("cookie-parser")
+const passport = require("passport")
+const { errorMiddleware } = require("./middlewares/errorMiddleware.js")
+const cors = require("cors")
