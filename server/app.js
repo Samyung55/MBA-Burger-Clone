ab0c0e223d55eeba248121a40c3c26b8b9d151a1
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-24 13:52:26 -0700

    Create app.js

---
+++ server/app.js
@@ -0,0 +1,61 @@
+const { express, urlencoded } = require("express")
+const dotenv = require("dotenv")
+const { connectPassport } = require("./utils/Provider.js")
+const session = require("express-session")
+const cookieParser = require("cookie-parser")
+const passport = require("passport")
+const { errorMiddleware } = require("./middlewares/errorMiddleware.js")
+const cors = require("cors")
+
+const app = express();
+export default app;
+dotenv.config({
+  path: "./config/config.env",
+});
+
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+  
+      cookie: {
+        secure: process.env.NODE_ENV === "development" ? false : true,
+        httpOnly: process.env.NODE_ENV === "development" ? false : true,
+        sameSite: process.env.NODE_ENV === "development" ? false : "none",
+      },
+    })
+  );
+
+  app.use(cookieParser());
+app.use(express.json());
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
+app.enable("trust proxy");
+
+connectPassport();
+
+// Routes
+const userRoute = require("./routes/user.js")
+const orderRoute = require("./routes/order.js")
+
+app.use("/api/v1", userRoute);
+app.use("/api/v1", orderRoute);
+
+// Using Error Middleware
+app.use(errorMiddleware);
