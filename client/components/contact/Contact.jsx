8a58f43e2b5b816d4da2f1b7168caa6a4b04212c
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-29 15:12:46 -0700

    Update Contact.jsx

--- client/components/contact/Contact.jsx
+++ client/components/contact/Contact.jsx
@@ -3,5 +3,24 @@ import { motion } from "framer-motion";
 import burger from "../../assets/burger2.png";
 
 const Contact = () => {
-
+    return (
+        <section className="contact">
+            <motion.form initial={{
+                x: "-100vw",
+                opacity: 0,
+            }}
+            animate={{
+                x: 0,
+                opacity: 1,
+            }}
+            transition={{
+                delay: 0.2
+            }}>
+                <h2>Contact Us</h2>
+                <input type="text" placeholder="Name" />
+                <input type="email" placeholder="Email" />
+                <textarea placeholder="Message" cols="30" rows="10"></textarea>
+            </motion.form>
+        </section>
+    )
 }
