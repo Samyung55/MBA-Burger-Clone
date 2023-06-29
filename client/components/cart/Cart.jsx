4f0d1c3d062cb6ff0d5f57e10f42372369370d1d
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-29 13:49:26 -0700

    Update Cart.jsx

--- client/components/cart/Cart.jsx
+++ client/components/cart/Cart.jsx
@@ -3,3 +3,18 @@ import { Link } from "react-router-dom";
 import burger1 from "../../assets/burger1.png";
 import burger2 from "../../assets/burger2.png";
 import burger3 from "../../assets/burger3.png";
+
+const CartItem = ({ value, title, img, increment, decrement }) => (
+    <div className="carItem">
+        <div>
+            <h4>{title}</h4>
+            <img src={img} alt="Item" />
+        </div>
+
+        <div>
+            <button onClick={decrement}>-</button>
+            <input type="number" readOnly value={value} />
+            <button onClick={increment}>+</button>
+        </div>
+    </div>
+)
