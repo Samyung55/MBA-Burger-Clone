4edb14df59247ef9171e22ba216be0db1afdb53b
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-29 14:52:02 -0700

    Update Cart.jsx

--- client/components/cart/Cart.jsx
+++ client/components/cart/Cart.jsx
@@ -18,3 +18,54 @@ const CartItem = ({ value, title, img, increment, decrement }) => (
         </div>
     </div>
 )
+
+const Cart = () => {
+    const increment = (item) => {};
+
+    const decrement = (item) => {};
+
+    return (
+        <section className="cart">
+            <main>
+                <CartItem title={"Cheese burger"}
+                img={burger1}
+                value={0}
+                increment={() => increment(1)}
+                decrement={() => decrement(1)}
+                />
+                <CartItem title={"Veg Cheese Burger"}
+                img={burger2}
+                value={0}
+                increment={() => increment(2)}
+                decrement={() => decrement(2)}
+                />
+                <CartItem title={"Cheese Burger with French Fries"}
+                img={burger3}
+                value={0}
+                increment={() => increment(3)}
+                decrement={() => decrement(3)}
+                />
+
+                <article>
+                    <div>
+                        <h4>Sub Total</h4>
+                        <p>₹{2000}</p>
+                    </div>
+                    <div>
+                        <h4>Tax</h4>
+                        <p>₹{2000 * 0.18}</p>
+                    </div>
+                    <div>
+                        <h4>Shipping Charges</h4>
+                        <p>₹{200}</p>
+                    </div>{" "}
+                    <div>
+                        <h4>Total</h4>
+                        <p>₹{2000 + 2000 * 0.18 + 200}</p>
+                    </div>
+                    <Link to="/shipping">Checkout</Link>
+                </article>
+            </main>
+        </section>
+    )
+}
