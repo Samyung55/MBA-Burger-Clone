4cf0e95ca300021e248cadc52b52b476e283d24c
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-29 14:57:13 -0700

    Update Shipping.jsx

--- client/components/cart/Shipping.jsx
+++ client/components/cart/Shipping.jsx
@@ -2,5 +2,24 @@ import React from "react";
 import { Country, State } from "country-state-city";
 
 const Shipping = () => {
-    
+    return (
+        <section className="shipping">
+            <main>
+                <h1>
+                    Shipping Details
+                </h1>
+                <form>
+                    <div>
+                        <label>H.No.</label>
+                        <input type="text" placeholder="House No."  />
+                    </div>
+                    <div>
+                        <label>City</label>
+                        <input type="text" placeholder="Enter City"  />
+                    </div>
+                    
+                </form>
+            </main>
+        </section>
+    )
 }
