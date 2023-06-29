32bf7ff480c771633b4f83b12e09df38ea05e6dc
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-29 13:46:27 -0700

    Create Cart.jsx

---
+++ client/components/cart/Cart.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import burger1 from "../../assets/burger1.png";
+import burger2 from "../../assets/burger2.png";
+import burger3 from "../../assets/burger3.png";
