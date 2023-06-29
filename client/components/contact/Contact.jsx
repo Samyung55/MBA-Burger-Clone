6766b3a823e34e915db74d8e8ebc5a94654beb19
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-29 15:14:47 -0700

    Update Contact.jsx

--- client/components/contact/Contact.jsx
+++ client/components/contact/Contact.jsx
@@ -22,6 +22,19 @@ const Contact = () => {
                 <textarea placeholder="Message" cols="30" rows="10"></textarea>
                 <button type="submit">Send</button>
             </motion.form>
+
+            <motion.div
+                className="formBorder"
+                initial={{
+                x: "100vw",
+                opacity: 0,
+                }}
+                animate={{
+                x: 0,
+                opacity: 1,
+                }}
+                transition={{ delay: 0.2 }}
+                ></motion.div>
         </section>
     )
 }
