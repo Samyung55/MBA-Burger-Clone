fd7c547d0f9c546b6617e046e286f00e9583bbcd
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-30 12:59:12 -0700

    Update Home.jsx

---
+++ client/components/home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Founder from "./Founder";
+import Menu from "./Menu";
+
+const Home = () => {
+    const options = {
+        initial: {
+            x: "-100%",
+            opacity: 0,
+        },
+        whileInView: {
+            x: 0,
+            opacity: 1,
+        },
+    };
+
+    return (
+        <>
+        
+        </>
+    )
+}
