771b649f49bdd3979714eb96493eacef76b7a8a2
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-30 13:18:37 -0700

    Update Header.jsx

--- client/components/layout/Header.jsx
+++ client/components/layout/Header.jsx
@@ -6,5 +6,24 @@ import { FaUser } from "react-icons/fa";
 import { motion } from "framer-motion";
 
 const Header = ({ isAuthenticated = false }) => {
-
+    return (
+        <nav>
+          <motion.div initial={{ x: "-100%" }} whileInView={{ x: 0 }}>
+            <IoFastFoodOutline />
+          </motion.div>
+    
+          <div>
+            <Link to="/">Home</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/about">About</Link>
+            <Link to="/cart">
+              <FiShoppingCart />
+            </Link>
+    
+            <Link to={isAuthenticated ? "/me" : "/login"}>
+              {isAuthenticated ? <FaUser /> : <FiLogIn />}
+            </Link>
+          </div>
+        </nav>
+      );
 }
