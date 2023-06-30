cb2041fa9ed9940e8c9aa514852f655e71aba40c
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-30 13:04:47 -0700

    Create Menu.jsx

---
+++ client/components/home/Menu.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import MenuCard from "./MenuCard";
+import burger1 from "../../assets/burger1.png";
+import burger2 from "../../assets/burger2.png";
+import burger3 from "../../assets/burger3.png";
+
+const Menu = () => {
+    const addToCartHandler = (itemNum) => {}
+}   
