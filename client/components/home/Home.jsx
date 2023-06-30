895abe45d298dbcac9acf9afb7aeb9225ce55e4b
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-06-30 13:03:34 -0700

    Update Home.jsx

--- client/components/home/Home.jsx
+++ client/components/home/Home.jsx
@@ -17,7 +17,34 @@ const Home = () => {
 
     return (
         <>
-        
+        <section className="home">
+            <div>
+                <motion.h1 {...options}>MBA Burger Wala</motion.h1>
+                <motion.p {...options}
+                transitions={{
+                    delay: 0.2,
+                }}>
+                    Give yourself a tasty burger.
+                </motion.p>
+            </div>
+
+            <motion.a href="#menu" initial={{
+                y: "-100%",
+                opacity: 0,
+            }}
+            whileInView={{
+                y: 0,
+                opacity: 1,
+            }}
+            transition={{
+                delay: 0.4,
+            }}>
+                Explore Menu
+            </motion.a>
+        </section>
+
+        <Founder />
+        <Menu />
         </>
     )
 }
