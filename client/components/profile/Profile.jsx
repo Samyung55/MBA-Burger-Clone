8fc54de4a88f46675b6f92f9a04b0fec898ce586
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-03 03:38:48 -0700

    Create Profile.jsx

---
+++ client/components/profile/Profile.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { motion } from "framer-motion";
+import me from "../../assets/founder.webp";
+import { Link } from "react-router-dom";
+import { MdDashboard } from "react-icons/md";
+
+const Profile = () => {
