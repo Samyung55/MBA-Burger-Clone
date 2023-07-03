4e2d55a9a5d5f8990ad8f9136a3ea08cb37b6eb6
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-03 03:35:34 -0700

    Create OrderDetails.jsx

---
+++ client/components/myOrders/OrderDetails.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const OrderDetails = () => {
+  return (
+    <section className="orderDetails">
+      <main>
+        <h1>Order Details</h1>
+        <div>
+          <h1>Shipping</h1>
+          <p>
+            <b>Address</b>
+            {"sjda 12-32ss dsad"}
+          </p>
+        </div>
+        <div>
+          <h1>Contact</h1>
+          <p>
+            <b>Name</b>
+            {"Abhishek"}
+          </p>
+          <p>
+            <b>Phone</b>
+            {2131232123}
+          </p>
+        </div>
