a1964e3011d60f15638d5a73c1b7ff1c7a331014
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-03 03:34:46 -0700

    Update myOrders.jsx

--- client/components/myOrders/myOrders.jsx
+++ client/components/myOrders/myOrders.jsx
@@ -5,5 +5,21 @@ import { AiOutlineEye } from "react-icons/ai";
 const MyOrders = () => {
   const arr = [1, 2, 3, 4];
 
-  
+  return (
+    <section className="tableClass">
+      <main>
+        <table>
+          <thead>
+            <tr>
+              <th>Order Id</th>
+              <th>Status</th>
+              <th>Item Qty</th>
+              <th>Amount</th>
+              <th>Payment Method</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+         
+  );
 }
