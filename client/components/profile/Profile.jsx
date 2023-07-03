c9a521346db66344e69e9361b948a67cdd4b6563
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-03 03:39:11 -0700

    Update Profile.jsx

--- client/components/profile/Profile.jsx
+++ client/components/profile/Profile.jsx
@@ -5,3 +5,16 @@ import { Link } from "react-router-dom";
 import { MdDashboard } from "react-icons/md";
 
 const Profile = () => {
+    const options = {
+        initial: {
+          y: "-100%",
+          opacity: 0,
+        },
+        animate: {
+          y: 0,
+          opacity: 1,
+        },
+      };
+
+      
+}
