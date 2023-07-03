e28bb15c55cb129a46f54203439279fc60afd09e
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-03 03:34:31 -0700

    Create myOrders.jsx

---
+++ client/components/myOrders/myOrders.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineEye } from "react-icons/ai";
+
+const MyOrders = () => {
+  const arr = [1, 2, 3, 4];
+
+  
+}
